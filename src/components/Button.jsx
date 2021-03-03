bed4a6530066ebff221124802e482fd734218ccf
simon <soelder@hotmail.com>
2021-03-03 16:40:18 +0100

    And some more

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styles from "./Button.module.css"
 
 const Button = ({ onClick, children }) => {
+  const [state, setState] = useState("")
+
+  useEffect(() => {
+    setState("test")
+  }, [])
 
   return (
     <button onClick={onClick} className={`${styles.button} ${styles.primary}`}>
