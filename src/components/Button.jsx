f38fded018cdd4aea462cb94c41ee96908855785
simon <soelder@hotmail.com>
2021-03-03 14:49:37 +0100

    Update eslint again

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import styles from './Button.module.css'
+import React from "react"
+import styles from "./Button.module.css"
 
-const Button = ({ onClick, children }) => (
-  <button onClick={onClick} className={`${styles.button} ${styles.primary}`}>
-    {children}
-  </button>
-)
+const Button = ({ onClick, children }) => {
+
+  return (
+    <button onClick={onClick} className={`${styles.button} ${styles.primary}`}>
+      {children}
+    </button>
+  )
+}
 
 export default Button
